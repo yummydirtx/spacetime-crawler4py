eb0942a16577a6d24c1c90637d143519ec261a01
Alex Frutkin <frutkinalexander@gmail.com>
2025-02-08 13:08:28 -0800

    Add functionality to remove existing report files on restart

--- launch.py
+++ launch.py
@@ -1,5 +1,6 @@
 from configparser import ConfigParser
 from argparse import ArgumentParser
+import os
 
 from utils.server_registration import get_cache_server
 from utils.config import Config
@@ -7,6 +8,17 @@ from crawler import Crawler
 
 
 def main(config_file, restart):
+    if restart:
+        if os.path.exists("report.txt"):
+            os.remove("report.txt")
+        if os.path.exists("page_hashes.txt"):
+            os.remove("page_hashes.txt")
+        if os.path.exists("subdomains.txt"):
+            os.remove("subdomains.txt")
+        if os.path.exists("longest_page.txt"):
+            os.remove("longest_page.txt")
+        if os.path.exists("visited_urls.txt"):
+            os.remove("visited_urls.txt")
     cparser = ConfigParser()
     cparser.read(config_file)
     config = Config(cparser)
